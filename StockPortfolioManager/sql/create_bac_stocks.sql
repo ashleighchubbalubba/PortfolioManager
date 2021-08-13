224e6ddc856714fb6597a4e2e82545ee4fc79230
ashleighchung <hichewash@gmail.com>
2021-08-13 15:56:51 -0400

    changed applicaition property URL and comented out the test class

--- StockPortfolioManager/sql/create_bac_stocks.sql
+++ StockPortfolioManager/sql/create_bac_stocks.sql
@@ -1,15 +1,18 @@
-CREATE TABLE bac_stocks7 (
-`Date` DATETIME NOT NULL,
-`Open` FLOAT NOT NULL,
-`High` FLOAT NOT NULL,
-`Low` FLOAT NOT NULL,
-`Close` FLOAT NOT NULL,
-`Adj Close` FLOAT NOT NULL,
-`Volume` FLOAT NOT NULL,
-PRIMARY KEY (`Date`));
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/BAC.csv'
-INTO TABLE bac_stocks7
+CREATE TABLE sandp (
+  `Symbol` VARCHAR(100) NOT NULL,
+  `Name` VARCHAR(100) NOT NULL,
+  `Sector` VARCHAR(100) NOT NULL,
+  `Price` FLOAT NOT NULL,
+  `Price/Earnings` FLOAT NOT NULL,
+  `Earnings/Share` FLOAT NOT NULL,
+  `52 Week High` FLOAT NOT NULL,
+  `52 Week Low` FLOAT NOT NULL,
+    PRIMARY KEY (`Date`));
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sample_data.csv'
+INTO TABLE sandp
 FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
+SELECT * FROM sandp;
 
 
--- StockPortfolioManager/src/test/java/com/citi/training/StockPortfolioManager/StockPortfolioManagerApplicationTests.java
+++ StockPortfolioManager/src/test/java/com/citi/training/StockPortfolioManager/StockPortfolioManagerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.citi.training.StockPortfolioManager;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StockPortfolioManagerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package StockPortfolioManager.src.test.java.com.citi.training.StockPortfolioManager;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class StockPortfolioManagerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
