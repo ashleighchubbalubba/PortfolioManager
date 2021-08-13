a86f06084acce48ca9c18cb158be5762cdd24e4a
ashleighchung <hichewash@gmail.com>
2021-08-13 12:23:58 -0400

    finished repo class

--- StockPortfolioManager/src/main/java/com/citi/training/StockPortfolioManager/repo/StockRepository.java
+++ StockPortfolioManager/src/main/java/com/citi/training/StockPortfolioManager/repo/StockRepository.java
@@ -1,9 +1,9 @@
-//package StockPortfolioManager.src.main.java.com.citi.training.StockPortfolioManager.repo;
-//
-//import java.util.Collection;
-//import com.citi.training.StockPortfolioManager.entities.Stocks;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MovieRepository extends JpaRepository<Stocks, Integer> {
-//    Collection<Stocks> findByTitle(String title);
-//}
+package StockPortfolioManager.src.main.java.com.citi.training.StockPortfolioManager.repo;
+
+import java.util.Collection;
+import com.citi.training.StockPortfolioManager.entities.Stocks;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepository extends JpaRepository<Stocks, Integer> {
+    Collection<Stocks> findByTitle(String title);
+}
